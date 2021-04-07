799af545faddb7b914f3bf45c9d12648cfdf1a3c
Spike Vinz Cruz <spikevinz@gmail.com>
2021-04-07 09:07:09 +0800

    move the before_action loggin requred callback to application controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,25 @@
 class ProfilesController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update  
+  end
+
+  private
+
+  def profile_param
+    params.permit(@profile)
+  end
+
 end
