c79a4331cbf121da7048027bd60730ce2425316e
Spike Vinz Cruz <spikevinz@gmail.com>
2021-04-09 14:37:46 +0800

    refactor avatar image to use helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar_img(user, size='128', pos='north')
+    if user.avatar.attached?
+    user.avatar.variant(resize_to_fill: [size, size, gravity: pos])
+    else
+     return "https://bulma.io/images/placeholders/256x256.png"
+    end
+  end
 end
