0bff555e8a89a6f45903e625c82f32eb3d73b3dd
Spike Vinz Cruz <spikevinz@gmail.com>
2021-04-14 09:43:51 +0800

    add basic welcome email with actionmailer

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
+   def create
+    super
+    UserMailer.welcome_email(resource).deliver unless resource.invalid?
+  end
+
   protected
 
     def after_update_path_for(resource)
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+   default from: 'fakebook@spike.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url  = new_user_session_url
+    mail(to: @user.email, subject: 'Welcome to Fakebook')
+  end
 end
